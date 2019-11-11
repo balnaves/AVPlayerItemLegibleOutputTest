b4dbfaa527067037673752644b23a95931ee916a
James Balnaves <james@rocketinsights.com>
2019-11-11 14:17:28 -0800

    Initial Commit

---
+++ AVPlayerItemLegibleOutputTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVPlayerItemLegibleOutputTest
+//
+//  Created by James Balnaves on 11/11/19.
+//  Copyright © 2019 James Balnaves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
