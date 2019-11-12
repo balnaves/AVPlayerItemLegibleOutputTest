63173f46bc693a2de2cd87b15620933290792167
James Balnaves <james@rocketinsights.com>
2019-11-11 17:57:24 -0800

    Hook up the AVPlayerViewController and add the AVPlayerItemLegibleOutput and delegate mechanism.

--- AVPlayerItemLegibleOutputTest/AppDelegate.swift
+++ AVPlayerItemLegibleOutputTest/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,8 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+        let audioSession = AVAudioSession.sharedInstance()
+           do {
+            try audioSession.setCategory(AVAudioSession.Category.playback)
+           }
+           catch {
+               print("Setting category to AVAudioSessionCategoryPlayback failed.")
+           }
+           return true
     }
 
     // MARK: UISceneSession Lifecycle
--- AVPlayerItemLegibleOutputTest/ViewController.swift
+++ AVPlayerItemLegibleOutputTest/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var playerView: UIView!
+    @IBOutlet weak var textView: UITextView!
+    
+    let playerVC = AVPlayerViewController()
+    let captionOutput = AVPlayerItemLegibleOutput()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        guard let url = URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8") else {
+            return
+        }
+        // Create an AVPlayer
+        let player = AVPlayer(url: url)
+        playerVC.player = player
+        
+        // Add the AVPlayerViewController view to our positioning  view
+        playerView.addSubview(playerVC.view)
+        playerVC.didMove(toParent: self)
+        
+        playerVC.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            playerVC.view.leadingAnchor.constraint(equalTo: playerView.leadingAnchor),
+            playerVC.view.trailingAnchor.constraint(equalTo: playerView.trailingAnchor),
+            playerVC.view.topAnchor.constraint(equalTo: playerView.topAnchor),
+            playerVC.view.bottomAnchor.constraint(equalTo: playerView.bottomAnchor)
+        ])
+        
+        // Set ourself as the delegate to AVPlayerItemLegibleOutput and we will
+        // get notified when a caption appears.
+        captionOutput.setDelegate(self, queue: DispatchQueue.main)
+        
+        // Turn off the AVPlayer's display of captions (if desired)
+        //captionOutput.suppressesPlayerRendering = true
+        
+        player.play()
+        player.currentItem?.add(captionOutput)
     }
-
-
 }
 
+extension ViewController: AVPlayerItemLegibleOutputPushDelegate {
+    func legibleOutput(_ output: AVPlayerItemLegibleOutput,
+                       didOutputAttributedStrings strings: [NSAttributedString],
+                       nativeSampleBuffers nativeSamples: [Any],
+                       forItemTime itemTime: CMTime) {
+        textView.attributedText = strings.first
+    }
+}
